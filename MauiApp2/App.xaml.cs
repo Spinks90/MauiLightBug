eeb0376216a88f773212ffcd8a3beae9ea03e786
Paul Spinks <63308868+Spinks90@users.noreply.github.com>
2022-10-04 16:57:04 +0100

    Add project files.

---
+++ MauiApp2/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace MauiApp2;
+
+public partial class App : Application
+{
+	public App()
+	{
+        Current.UserAppTheme = AppTheme.Light;
+
+        InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
